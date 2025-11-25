a66628ba5d4dc5750586937a6d9c74e5b24067ca
cmcrskdiaid <ofpptid0@gmail.com>
2025-11-25 18:11:50 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-$host = "192.168.1.100";   // adresse IP du serveur MySQL
+$host = "192.168.44.131";   // adresse IP du serveur MySQL
 $user = "appuser";         // utilisateur MySQL dédié
 $pass = "motdepasse";      // mot de passe de l'utilisateur
 $db   = "entreprise";      // nom de la base de données
@@ -9,4 +9,5 @@ $conn = new mysqli($host, $user, $pass, $db);
 if ($conn->connect_error) {
     die("Connexion échouée: " . $conn->connect_error);
 }
+
 ?>
